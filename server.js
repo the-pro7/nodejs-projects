abe56071c1c5d4b818d559018c67117d832f268a
Emmanuel Opoku-Ameyaw <110876611+the-pro7@users.noreply.github.com>
2023-07-18 16:03:51 +0000

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,9 @@
+const http = require("http")
+const path = require("path")
+const fs = require("fs")
+const fsPromises = require("fs/promises")
 
+const server = http.createServer((req, res) => {
+  res.writeHead(200, {"Content-type": "text/html"})
+  res.end("Hello)
+})
