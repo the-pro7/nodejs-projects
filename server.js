f4b2bfd4f2c66603833b1e41a8295ae05a571588
Emmanuel Opoku-Ameyaw <110876611+the-pro7@users.noreply.github.com>
2023-07-18 16:04:42 +0000

    Update server.js

--- server.js
+++ server.js
@@ -7,3 +7,8 @@ const server = http.createServer((req, res) => {
   res.writeHead(200, {"Content-type": "text/html"})
   res.end("Hello)
 })
+
+
+server.listen(5000, () => {
+  console.log(`Server running on port 5000...`)
+})
